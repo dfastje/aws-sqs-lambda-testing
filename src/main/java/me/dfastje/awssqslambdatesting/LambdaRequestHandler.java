5c173cf8a2907f747a73a11686b46bfc87951b44
dfast <dfastje@gmail.com>
2018-03-14 22:12:12 -0400

    initial commit

---
+++ src/main/java/me/dfastje/awssqslambdatesting/LambdaRequestHandler.java
@@ -0,0 +1,12 @@
+package me.dfastje.awssqslambdatesting;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaRequestHandler implements RequestHandler {
+
+    @Override
+    public Object handleRequest(Object object, Context context) {
+        return null;
+    }
+}
